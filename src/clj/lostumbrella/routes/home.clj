1dbc3db53d39598e8fa0626dce814e3ac91f61e5
scouri <eneblank19@protonmail.com>
2022-06-08 02:50:36 +0000

    Update home.clj

--- src/clj/lostumbrella/routes/home.clj
+++ src/clj/lostumbrella/routes/home.clj
@@ -1,21 +1,23 @@
-(ns lostumbrella.routes.home
+(ns hanakumori.routes.home
   (:require
-   [lostumbrella.layout :as layout]
+   [hanakumori.layout :as layout]
    [clojure.java.io :as io]
-   [lostumbrella.middleware :as middleware]
+   [hanakumori.middleware :as middleware]
    [ring.util.response]
    [ring.util.http-response :as response]))
 
 (defn home-page [request]
   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
-(defn about-page [request]
-  (layout/render request "about.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
-
+(defn posts-page [request]
+  (layout/render request "posts.html" {:docs (-> "docs/posts.md" io/resource slurp)}))
+  (defn lily58-post [request]
+    (layout/render request "lily58.html" {:docs (-> "docs/lily58.md" io/resource slurp)}))
 (defn home-routes []
   [ "" 
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/about" {:get about-page}]])
+   ["/posts" {:get posts-page}]
+   ["/lily58" {:get lily58-post}]])
 
