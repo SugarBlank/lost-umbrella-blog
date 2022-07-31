2e1327484b2c4bf57d797245d9e6051e05f1c11b
scouri <eneblank19@protonmail.com>
2022-07-31 07:28:48 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,15 +1,19 @@
-(defproject hanakumori "0.1.0-SNAPSHOT"
-  :description "Blog"
+(defproject akaiyuki "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
                  [stasis "2.5.1"]
                  [markdown-clj "1.11.1"]
                  [hiccup "1.0.5"]
                  [ring "1.9.5"]
+                 [digest "1.4.9"]
                  [optimus "2022-02-13"]
-                 [enlive "1.1.6"]]
-  :ring {:handler hanakumori.core/server}
-  :profiles {:dev {:plugins [[lein-ring "0.12.5"]]}}
-  :repl-options {:init-ns hanakumori.core})
+                 [enlive "1.1.6"]
+                 [babashka/fs "0.1.6"]
+                 [clygments "2.0.2"]]
+  :ring {:handler akaiyuki.core/app}
+  :profiles {:dev {:plugins [[lein-ring "0.12.6"]]}}
+  :repl-options {:init-ns akaiyuki.core}
+  :aliases {"build-site" ["run" "-m" "akaiyuki.core/export"]})
