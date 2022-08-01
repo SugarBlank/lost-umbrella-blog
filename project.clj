2beba7f36b7523d84bf225dd88a3689f625a5369
scouri <eneblank19@protonmail.com>
2022-08-01 04:46:43 +0000

    versioning etc

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject akaiyuki "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject akaiyuki "1.0"
+  :description "Blog website"
+  :url "https://akaiyuki.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.0"]
