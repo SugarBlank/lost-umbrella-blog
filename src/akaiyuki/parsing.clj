7edf32339ee1de65a1dbed55ffffd99175691a8a
scouri <eneblank19@protonmail.com>
2022-08-01 04:45:31 +0000

    styleguides

--- src/akaiyuki/parsing.clj
+++ src/akaiyuki/parsing.clj
@@ -5,7 +5,7 @@
             [clojure.java.io :as io]
             [akaiyuki.misc :refer [source-dir key-to-html]]))
 
-(defn parse-post
+(defn- parse-post
   [file]
   (when-let [[_ year month day name] (re-matches #"([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)\.md" (fs/file-name file))]
     (with-open [rdr (java.io.PushbackReader. (io/reader (fs/file file)))]
