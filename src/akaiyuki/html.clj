58f451cdf7983bf5839fbcdbcbd6929f66cf6f12
scouri <eneblank19@protonmail.com>
2023-03-23 01:19:08 +0000

    add custom font

--- src/akaiyuki/html.clj
+++ src/akaiyuki/html.clj
@@ -14,6 +14,7 @@
           [:meta {:name "viewport"
                   :content "width=device-width, initial-scale=1.0"}]
           [:link {:type "text/css" :href "/public/css/style.css" :rel "stylesheet"}]
+          [:style "body { font-family: 'CozetteVector', sans-serif;}"]
           [:body
            [:div {:class "header"}
             [:div {:class "name"}
